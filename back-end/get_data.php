9437d2c9a0b75e0cb9396c0df07a095d7243f355
Javier Quintero <javierq@TruJavy.local>
2017-07-13 21:03:18 -0700

    set up api

--- back-end/get_data.php
+++ back-end/get_data.php
@@ -2,7 +2,26 @@
 date_default_timezone_set('America/Los_Angeles');
 $db = new mysqli('ec2-54-68-139-60.us-west-2.compute.amazonaws.com', 'javierq', 'abcd1234');
 
-
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    // return individual test data
+    if(isset($_GET['id'])){
+        $cabId = $_Get['id'];
+        $sql = "select *
+        from read_port.fact_readings
+        where cabinet_id=$cabId
+        order by date_key";
+        $data = $db->query($sql);
+        if($data){
+            while($row = $data->fetch_assoc()){
+                $dk = strtotime($row['date_key'])*1000;
+                $returnData['name'] = $row['cabinet_name'];
+                $metric = $row['metric'];
+                $returnData['time_series'][$metric][] = array($dk,floatval($row['metric_value']));
+            }
+        }
+        echo(json_encode($returnData));
+    }
+}
 
 
  ?>
