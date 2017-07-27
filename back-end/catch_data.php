19923dfc482619ef0b3c21f7d01cacf6d6c2a384
Paal Wik <prwik@MacBook-Air.local>
2017-07-27 12:16:27 -0700

    config update

--- back-end/catch_data.php
+++ back-end/catch_data.php
@@ -1,43 +1,21 @@
 <?php
+include 'config.php';
+
 date_default_timezone_set('America/Los_Angeles');
-$db = new mysqli('69.195.124.145', 'coinsurf_paal', 'HK0h0.NZ9+q]','coinsurf_hhouse', 3306 );
+$db = new mysqli('69.195.124.145', $user, $pass, $db_name, $db_port );
 
     if($_SERVER['REQUEST_METHOD']=='POST'){
-    // return individual test data
-        if ( $_POST ) {
-            foreach ( $_POST as $key => $value ) {
-                echo "key: ".$key."- Value:".$value."<br />";
-            }
+        if(isset($_POST)){
+            $data = json_decode(file_get_contents('php://input'));
+            $sql = "INSERT INTO coinsurf_hhouse.userinput (id, user, water, grow, micro, bloom, roots, logdate) VALUES ( NULL,'" . $data->username . "', '".$data->data->water."', '".$data->data->grow."', '".$data->data->micro."', '".$data->data->bloom."', '".$data->data->roots."', CURRENT_TIMESTAMP)";
+            $db->query($sql);
+            echo($db->error);
         }
     }
-    elseif(isset($_GET['all'])){
-        $sql = "
-        select a.cabinet_id, b.cabinet_name, b.metric, b.metric_value, b.date_key
-        from
-        (select cabinet_id, max(date_key) as date_key
-        from read_port.fact_readings
-        where metric in ('IAH','IAT','pH','WT')
-        group by 1) a
-        left join
-        (select cabinet_id, date_key, cabinet_name, metric, metric_value
-        from read_port.fact_readings
-        where metric in ('IAH','IAT','pH','WT')
-        ) b on a.cabinet_id=b.cabinet_id and a.date_key=b.date_key
-        order by 3
-        ";
-        $data = $db->query($sql);
-        if($data){
-            while($row = $data->fetch_assoc()){
-                $name = $row['cabinet_name'];
-                $metric = $row['metric'];
-                $returnData[$name]['id'] = $row['cabinet_id'];
-                $returnData[$name]['data'][$metric] = floatval($row['metric_value']);
-                $returnData[$name]['time'] = $row['date_key'];
-            }
-        }
-        echo(json_encode($returnData));
-    }
-}
 
+?>
 
- ?>
+<form method="post" action="catch_data.php">
+    <input type="hidden" name="user" value="bert" />
+    <button>Go to user 123</button>
+</form>
