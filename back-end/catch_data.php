e5a5f1e4126bfd4cde09a55e6e50dd01d1bd65df
Paal Wik <paal.wik@gmail.com>
2017-08-06 17:48:27 -0700

    update

--- back-end/catch_data.php
+++ back-end/catch_data.php
@@ -6,7 +6,7 @@ $db = new mysqli( $db_host, $user, $pass, $db_name, $db_port );
 
     if($_SERVER['REQUEST_METHOD']=='POST'){
         if(isset($_POST)){
-            $data = json_decode(file_get_contents('php://input'), true);
+            $data = filter_var_array(json_decode(file_get_contents('php://input'), true), FILTER_SANITIZE_SPECIAL_CHARS);
             $userdata = $data["data"];
             $sql = sprintf("INSERT INTO coinsurf_hhouse.userinput (id, user, unit, water, grow, micro, bloom, roots, light, height, comment, logdate) VALUES ( NULL,'%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
                 $data["user"],
